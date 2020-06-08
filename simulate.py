2388402d81eb64d3eb52c0e908fac7d5e7fd43a6
Yoni Maltsman <46268861+ymaltsman@users.noreply.github.com>
2020-06-08 14:57:06 -0500

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 from locust import locust
 from gridpoint import gridpoint
-def simulate(gens = 100, iters = 100, numrows = 1, rowlen = 100, R = 50, cutoff = 10, C = 30, G = 30):
+def simulate(gens = 100, iters = 100, numrows = 1, rowlen = 100, R = 50, cutoff = 10, C = 30, G = 30, genData = False):
     """
     Parameters
     --------------------------------
@@ -12,7 +12,7 @@ def simulate(gens = 100, iters = 100, numrows = 1, rowlen = 100, R = 50, cutoff 
     R: number of resources
     cutoff: the number of locusts that die off and are born each generation
     C, G: number of locusts in control and gregarizing groups, respectively
-    genData: a boolean that dictates whether or not generational level data will be stored and returned
+    genData: a boolean that dictates whether or not generational level data about the grid will be stored and returned -- not yet in use
 
     Outputs
     ---------------------------------
@@ -20,6 +20,8 @@ def simulate(gens = 100, iters = 100, numrows = 1, rowlen = 100, R = 50, cutoff 
     grid: the 'grid' ie the list of gridpoints as instances of the gridpoint class
     props: a list that keeps track of the proportion of gregarizing locusts in 
     the population
+    gridData: returns generational level grid data if set to true
+    locustData: returns generational level data about locusts
     """
 
     N=C+G
@@ -68,11 +70,13 @@ def simulate(gens = 100, iters = 100, numrows = 1, rowlen = 100, R = 50, cutoff 
             place = grid[i][x]
             clocust = locust(place, 0)
             rowlocusts.append(clocust)
+            clocust.place.newlocust()
         for g in range(G):
             x = int(np.random.uniform()*len(grid[i]))
             place = grid[i][x]
             glocust = locust(place, 1)
             rowlocusts.append(glocust)
+            glocust.place.newlocust()
         locusts.append(rowlocusts)
 
     #randomly reorder each list of locusts
@@ -83,14 +87,28 @@ def simulate(gens = 100, iters = 100, numrows = 1, rowlen = 100, R = 50, cutoff 
     props = []
     props.append(G/N)
     
+    #gridData will track the distribution of locusts and resources on the grid
+    gridData = []
+    locustData =[]
+    print(sum([x.locusts for x in grid[0]]))
     for g in range(gens):
         greg=0
         cont=0
-        for r in range(len(locusts)):
-            #run through a single gen
-            for i in range(iters):
+        gridr=[]
+        locustr=[]
+        
+        #run through a single gen
+        for i in range(iters):
+            for r in range(len(locusts)):
                 for l in range(len(locusts[r])):
                     locusts[r][l].iterate(locusts[r], i, grid[r])
+            gridr.append([x.locusts for x in grid[0]])
+            locustr.append([l.phase for l in locusts[0]])
+        gridData.append(gridr)
+        locustData.append(locustr)
+
+        for r in range(len(locusts)):
+            
             #remove the worst performing locusts
             for n in range(cutoff):
                 min = locusts[r][0].efficiency
@@ -99,7 +117,10 @@ def simulate(gens = 100, iters = 100, numrows = 1, rowlen = 100, R = 50, cutoff 
                     if locusts[r][l].efficiency < min:
                         min = locusts[r][l].efficiency
                         worst = locusts[r][l]
+                worst.place.loselocust()
                 locusts[r].remove(worst)
+            
+            #best locusts reproduce
             for n in range(cutoff):
                 max = locusts[r][0].efficiency
                 best = locusts[r][0]
@@ -109,13 +130,17 @@ def simulate(gens = 100, iters = 100, numrows = 1, rowlen = 100, R = 50, cutoff 
                         best = locusts[r][l]
                 baby = locust(best.place, best.group, best.phase)
                 locusts[r].append(baby)
+                baby.place.newlocust()
+        
+        #updating proportion of gregarizing locusts
         for l in range(len(locusts[r])):
             if locusts[r][l].group == 0:
                 cont += 1
             else:
                 greg += 1
         props.append(greg/N)
-    return locusts, grid, props
+
+    return [locusts, grid, props, gridData, locustData]
 
 
 
